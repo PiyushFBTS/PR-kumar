1cb2164955afc30eb374dfadbcf22ab8f546c474
m-piyush <piyushmalviya1969@gmail.com>
2026-06-17 17:59:02 +0530

    feat-header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -157,7 +157,7 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 border-b border-white/10 bg-black text-white">
       <Container className="flex items-center justify-between py-4">
-        <Link href="/" className="brand-script text-2xl text-white sm:text-3xl">
+        <Link href="/" className="brand-script text-2xl text-white sm:text-2xl">
           {site.name}
         </Link>
 
